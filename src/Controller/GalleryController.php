66a492b8ac4b7aea1c1dc2f2439b6a59aa66e574
Gui <gcharmassonjm@gmail.com>
2019-06-24 19:38:23 +0200

    GalleryController

---
+++ src/Controller/GalleryController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\GalleryImage;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GalleryController extends PagesController
+{
+    /**
+     * @Route("/galerie", name="show_gallery")
+     */
+    public function showGallery()
+    {
+        $repository = $this->getDoctrine()->getRepository(GalleryImage::class);
+        $gallery = $repository->findAll();
+        return $this->render('pages/gallery.html.twig', compact('gallery'));
+    }
+}
